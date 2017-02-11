c943f5aabef2afb9798d01cc31044d9dbe273958
Wesley Hsieh <wesley.y.hsieh@gmail.com>
2017-02-10 22:26:47 -0800

    Fixed HOG features

--- src/deep_lfd/learning_driving/learner.py
+++ src/deep_lfd/learning_driving/learner.py
@@ -53,7 +53,20 @@ class Learner(object):
 		return downsampled
 
 	def extract_HOG(self, state):
-		hog = cv2.HOGDescriptor()
+		winSize = (128,128)
+		blockSize = (16,16)
+		blockStride = (8,8)
+		cellSize = (8,8)
+		nbins = 9
+		derivAperture = 1
+		winSigma = 4.
+		histogramNormType = 0
+		L2HysThreshold = 2e-01
+		gammaCorrection = 0
+		nlevels = 64
+		hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+                        histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+		# hog = cv2.HOGDescriptor()
 		h = np.ravel(hog.compute(state))
 		return h
 
